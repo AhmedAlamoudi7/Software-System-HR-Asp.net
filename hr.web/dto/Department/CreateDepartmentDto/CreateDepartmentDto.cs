a25f8bf403ce20f24e32b2df81e40c25fd8f5739
Eng.Ahmed ALAmoudi <76726399+AhmedAlamoudi7@users.noreply.github.com>
2021-09-23 08:55:31 +0300

    Update Projects Added ViewBags Message Project Version 2.0Push Project V2.0

--- hr.web/dto/Department/CreateDepartmentDto/CreateDepartmentDto.cs
+++ hr.web/dto/Department/CreateDepartmentDto/CreateDepartmentDto.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace hr.web.ViewModel.Department
+namespace hr.web.dto.Department.CreateDepartmentDto
 {
-    public class CreateDepartemntViewModel
+    public class CreateDepartmentDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
         public List<int> Employees { get; set; }
+
     }
 }
--- hr.web/dto/Department/UpdateDepartmentDto/UpdateDepartmentDto.cs
+++ hr.web/dto/Department/UpdateDepartmentDto/UpdateDepartmentDto.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace hr.web.ViewModel.Department
+namespace hr.web.dto.Department.UpdateDepartmentDto
 {
-    public class UpdateDepartmentViewModel
+    public class UpdateDepartmentDto
     {
         public int Id { get; set; }
         public string Name { get; set; }
